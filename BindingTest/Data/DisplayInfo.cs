f6f8627262918d48d4e6d48a517bf56bcec47595
Franck Espinosa <franck@espinosa.eu>
2023-01-24 20:26:42 +0100

    Add project files.

---
+++ BindingTest/Data/DisplayInfo.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BindingTest.Data;
+
+public class DisplayInfo : ObservableObject
+{
+    private string deviceName = string.Empty;
+    public string DeviceName
+    {
+        get { return deviceName; }
+        set { deviceName = value; OnPropertyChanged(nameof(DeviceName)); }
+    }
+
+    private bool primary;
+    public bool Primary
+    {
+        get { return primary; }
+        set { primary = value; OnPropertyChanged(nameof(Primary)); }
+    }
+}
---
+++ BindingTest/Data/DisplayService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using WindowsDisplayAPI;
+
+namespace BindingTest.Data;
+
+public class DisplayService
+{
+    public ObservableCollection<DisplayInfo> ScreenCollection { get; set; } = new();
+
+    public DisplayService()
+    {
+        foreach (Display display in Display.GetDisplays())
+        {
+            ScreenCollection.Add(new DisplayInfo
+            {
+                DeviceName = display.DisplayName,
+                Primary = display.IsGDIPrimary,
+            });
+        }
+    }
+}
